8b048853178aea0e492981af0dc2a0044422c3cf
Melis <Melis@DESKTOP-JRS35T2>
2024-10-23 00:35:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class BaseKisi
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+
+    public void KisiBilgileriYazdir()
+    {
+        Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+    }
+}
+
+public class Ogrenci : BaseKisi
+{
+    public string OgrenciNumarasi { get; set; }
+
+    public void OgrenciBilgileriYazdir()
+    {
+        Console.WriteLine($"Öğrenci Numarası: {OgrenciNumarasi}");
+        KisiBilgileriYazdir(); // BaseKisiden gelen metodu cagiriyoruz
+    }
+}
+
+public class Ogretmen : BaseKisi
+{
+    public decimal MaasBilgisi { get; set; }
+
+    public void OgretmenBilgileriYazdir()
+    {
+        Console.WriteLine($"Maaş Bilgisi: {MaasBilgisi}");
+        KisiBilgileriYazdir(); // BaseKisiden gelen metodu cagiriyoruz
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ogrenci nesnesi tanimlama ve deger atama
+        Ogrenci ogrenci = new Ogrenci
+        {
+            Ad = "Mahir",
+            Soyad = "Yıldız",
+            OgrenciNumarasi = "614"
+        };
+
+        // ogretmen nesnesi tanimlama ve deger atama
+        Ogretmen ogretmen = new Ogretmen
+        {
+            Ad = "Mia",
+            Soyad = "Godsgiven",
+            MaasBilgisi = 36200
+        };
+
+        // bilgileri konsol ekranina yazdirma
+        ogrenci.OgrenciBilgileriYazdir();
+        Console.WriteLine(); // bos satir
+        ogretmen.OgretmenBilgileriYazdir();
+    }
+}
